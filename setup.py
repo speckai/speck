e255f9eea661937019eea25139565abb3014f957
Lucas Jaggernauth <lukejagg@gmail.com>
2023-11-19 22:17:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,17 +9,19 @@ def readme():
 
 setup(
     name="python-speck",
+    packages=["speck"],
     version="0.0.0",
     description="Speck - Unified observability platform for production LLM apps",
     long_description=readme(),
     long_description_content_type="text/markdown",
+    keywords=["speck", "openai", "llm", "ai", "chat", "bot", "gpt", "gpt-3"],
     url="https://github.com/speckai/speck",
+    download_url="https://github.com/speckai/speck/archive/refs/tags/v0.0.0.tar.gz",
     author="Lucas Jaggernauth",
     author_email="luke@speck.chat",
     classifiers=[
         "Programming Language :: Python :: 3.12",
     ],
-    packages=["py-automl"],
     include_package_data=True,
     install_requires=[],
 )
