21f0caceb7652cb15e35d12e8b9677ad353bcc01
Lucas Jaggernauth <lukejagg@gmail.com>
2023-12-09 14:48:19 -0600

    Added test for openai wrapper

---
+++ src/python/tests/openai_wrapper.py
@@ -0,0 +1,33 @@
+import os
+
+with open(".env") as f:
+    lines = f.readlines()
+    for line in lines:
+        key, value = line.split("=")
+        os.environ[key] = value
+
+import os
+
+from speck import OpenAI
+
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+
+kwargs = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {"role": "system", "content": "hi"},
+        {"role": "user", "content": "hi"},
+    ],
+}
+
+for stream in [False, True]:
+    completion = client.chat.completions.create(
+        **kwargs,
+        stream=stream,
+    )
+
+    if stream:
+        for chunk in completion:
+            print(chunk.choices[0].delta.content)
+    else:
+        print(completion.choices[0].message)
