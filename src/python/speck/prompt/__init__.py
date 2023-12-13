3b6ba5550ecbda8871f9ebb23c536fc5596d12d1
Lucas Jaggernauth <lukejagg@gmail.com>
2023-12-12 18:25:30 -0600

    Initial promptfile format

---
+++ src/python/speck/prompt/__init__.py
@@ -0,0 +1,1 @@
+from ._promptfile import Promptfile
---
+++ src/python/speck/prompt/_promptfile.py
@@ -0,0 +1,14 @@
+class Promptfile:
+    """A class for loading and parsing promptfiles."""
+
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+        self.promptfile = self._load_promptfile()
+
+    def _load_promptfile(self) -> dict:
+        """Load the promptfile from the filepath."""
+        return {}
+
+    def _parse_promptfile(self) -> dict:
+        """Parse the promptfile."""
+        return {}
