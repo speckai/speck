6b949d5923ae67aeb145f4dcd07a912c6487df90
Lucas Jaggernauth <lukejagg@gmail.com>
2023-12-28 17:35:38 -0600

    Updated metadata timestamp to tick

--- src/python/speck/logs/metadata.py
+++ src/python/speck/logs/metadata.py
@@ -1,14 +1,16 @@
 import datetime
 import os
 import threading
+from typing import Any
 
 
-def generate_metadata_dict() -> dict[str, str]:
-    metadata: dict[str, str] = {
+def generate_metadata_dict() -> dict[str, Any]:
+    metadata: dict[str, Any] = {
         "thread": threading.get_ident(),
         "process": os.getpid(),
-        "timestamp": datetime.datetime.now(datetime.UTC).strftime(
-            "%Y-%m-%d %H:%M:%S.%f"
-        )[:-3],
+        # "timestamp": datetime.datetime.now(datetime.UTC).strftime(
+        #     "%Y-%m-%d %H:%M:%S.%f"
+        # )[:-3],
+        "timestamp": datetime.datetime.now().timestamp(),
     }
     return metadata
