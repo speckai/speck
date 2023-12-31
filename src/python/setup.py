c315c681ede9ecffec83c38b39ddc3c8b15faaba
Lucas Jaggernauth <lukejagg@gmail.com>
2023-12-31 16:21:30 -0600

    Updated PyPI metadata

--- src/python/setup.py
+++ src/python/setup.py
@@ -25,7 +25,7 @@ setup(
     name="speck",
     packages=find_packages(exclude=["tests", "tests.*"]),
     version="0.0.7",
-    description="Speck - LLM observability framework.",
+    description="Speck - The dev framework for LLM apps.",
     long_description=readme(),
     long_description_content_type="text/markdown",
     keywords=[
@@ -42,9 +42,10 @@ setup(
         "replicate",
     ],
     url="https://github.com/speckai/speck",
-    download_url="https://github.com/speckai/speck/archive/refs/tags/v0.0.6.tar.gz",
-    author="Lucas Jaggernauth",
-    author_email="luke@speck.chat",
+    download_url="https://github.com/speckai/speck/archive/refs/tags/v0.0.7.tar.gz",
+    homepage="https://speck.chat",
+    author="",
+    author_email="Lucas Jaggernauth <luke@speck.chat>, Raghav Pillai <raghav@speck.chat>",
     classifiers=[
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.11",
